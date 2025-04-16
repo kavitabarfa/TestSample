efc07b1af8119f5547ca3ff8f841c9a56ef24ded
seervkx <kavita.seervi@abbott.com>
2025-04-16 15:08:09 +0530

    Sample Test

--- TestScriptJenkin/src/main/java/TestFileHello.java
+++ TestScriptJenkin/src/main/java/TestFileHello.java
@@ -2,6 +2,6 @@ public class TestFileHello {
 
         public static void main(String[] args) {
             System.out.println("Helloooooooooooo Worldddddddddddddddddddd!");
+            System.out.println("hiiiiiii Worldddddddddddddddddddd!");
         }
     }
-
